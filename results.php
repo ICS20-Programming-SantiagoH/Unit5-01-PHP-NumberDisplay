ccecb612900ecc3939bcabaf866481fc4d45a1ed
santiagohewett <124379638+santiagohewett@users.noreply.github.com>
2023-05-09 15:16:59 +0000

    final

--- results.php
+++ results.php
@@ -11,22 +11,26 @@ $message = "";
 //Display if user did not enter anything
 if ($max=="" || $min=="") {
   $message ="Please Enter a Min and/or Max";
+  
 }
 
 // Display if user put a invalid min
 else if ($counter > $max) {
   $message ="Please enter a Min that is Less than the Max";
+  
 }
 
 //While for statement for number display
 else {
-  while ($counter <= $max){
+  while ($counter <= $max) {
 //Number with line break
     $message = $message . $counter . ", ";
 //Add to counter
-    $counter = $counter ++;
+    $counter = $counter +1;
+    
   }
 }
 
 echo $message;
+
 ?>
